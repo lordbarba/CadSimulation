87b3883c0b17a27109cb713d30c61f1c0fc247df
Leonardo Zambonelli <leonardo.zambonelli@gmail.com>
2025-02-20 22:45:31 +0100

    Developed Iteration 1

--- CadSimulation/Program.cs
+++ CadSimulation/Program.cs
@@ -1,7 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CadSimulation;
+using CommandLine;
+using System.Runtime.CompilerServices;
+using System.Text;
 
 List<Shape> shapes = new List<Shape>();
+string targetFilename = string.Empty;
+
+Parser.Default.ParseArguments<CommandLineOptions>(args)
+           .WithParsed(o =>
+           {
+               targetFilename = o.Path;
+           });
 
 while (true)
 {
@@ -13,6 +23,8 @@ while (true)
 "   'r': insert a rectangle\n" +
 "   'l': list all inserted shapes\n" +
 "   'a': all shapres total area\n" +
+"   'k': store data\n" +
+"   'w': get stored data\n" +
 "   'q': quit");
 
     var k = Console.ReadKey(true);
@@ -61,6 +73,24 @@ while (true)
             var radius = Int32.Parse(Console.ReadLine()!);
             shape = new Circle(radius); // Console.WriteLine("Circle");
             break;
+        case 'k':
+            if (string.IsNullOrEmpty(targetFilename))
+            {
+                Console.WriteLine("Path is not set");
+                continue;
+            }
+            Console.WriteLine($"Storing data to {targetFilename}:\t");
+            executeStoreData();
+            break;
+        case 'w':
+            if (!System.IO.File.Exists(targetFilename))
+            {
+                Console.WriteLine("File does not esist ");
+                continue;
+            }
+            Console.WriteLine($"Retrieving data from {targetFilename}:\t");
+            executoRetrieveData();
+            break;
         case 'a':
             {
                 double area = 0;
@@ -75,6 +105,63 @@ while (true)
 
 }
 
+void executoRetrieveData()
+{
+    string[] sLines=System.IO.File.ReadAllLines(targetFilename);
+    shapes.Clear();
+    foreach (var item in sLines)
+    {
+        if (string.IsNullOrEmpty(item)) continue;
+        string[] sValues = item.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        if (sValues.Length == 0) continue;
+        if (sValues[0] == "C")
+        {
+            shapes.Add(new Circle(Int32.Parse(sValues[1])));
+        }
+        else if (sValues[0] == "S")
+        {
+            shapes.Add(new Square(Int32.Parse(sValues[1])));
+        }
+        else if (sValues[0] == "R")
+        {
+            shapes.Add(new Rectangle(Int32.Parse(sValues[1]), Int32.Parse(sValues[2])));
+        }
+        else if (sValues[0] == "T")
+        {
+            shapes.Add(new Triangle(Int32.Parse(sValues[1]), Int32.Parse(sValues[2])));
+        }
+    }
+}
+
+void executeStoreData()
+{
+    StringBuilder sb = new StringBuilder();
+    foreach (var s in shapes)
+    {
+        if (s == null) continue;
+        if (s is Circle shapeCirle)
+        {
+            sb.Append("C").Append(" ").AppendLine(shapeCirle.Radius.ToString());
+        }
+        else if (s is Square shapeSquare)
+        {
+            sb.Append("S").Append(" ").AppendLine(shapeSquare.Side.ToString());
+        }
+        else if (s is Rectangle shapeRectangle)
+        {
+            sb.Append("R").Append(" ").Append(shapeRectangle.Height).Append(" ").AppendLine(shapeRectangle.Width.ToString());
+        }
+        else if (s is Triangle shapeTriangle)
+        {
+            sb.Append("T").Append(" ").Append(shapeTriangle.Base).Append(" ").AppendLine(shapeTriangle.Height.ToString());
+        }
+        sb.AppendLine();
+    }
+
+    System.IO.File.WriteAllText(targetFilename, sb.ToString());
+}
+
+
 namespace CadSimulation
 {
     internal interface Shape
@@ -85,6 +172,7 @@ namespace CadSimulation
     internal class Square : Shape
     {
         readonly int _side;
+        public int Side { get => _side; }
         public Square(int side)
         {
             _side = side;
@@ -102,25 +190,29 @@ namespace CadSimulation
     internal class Rectangle : Shape
     {
         readonly int _height;
-        readonly int _weidth;
+        readonly int _width;
+
+        public int Height { get => _height; }
+        public int Width { get => _width; }
         public Rectangle(int height, int weidth)
         {
             _height = height;
-            _weidth = weidth;
+            _width = weidth;
         }
         double Shape.area()
         {
-            return _height * _weidth;
+            return _height * _width;
         }
 
         void Shape.descr()
         {
-            Console.WriteLine($"Rectangle, height: {_height}, weidth: {_weidth}");
+            Console.WriteLine($"Rectangle, height: {_height}, weidth: {_width}");
         }
     }
     internal class Circle : Shape
     {
         int _radius;
+        public int Radius { get => _radius;  }
         public Circle(int radius)
         {
             _radius = radius;
@@ -140,6 +232,8 @@ namespace CadSimulation
     {
         int _base;
         int _height;
+        public int Base { get => _base;  }
+        public int Height { get => _height; }
         public Triangle(int b, int h)
         {
             _base = b;
@@ -155,3 +249,13 @@ namespace CadSimulation
         }
     }
 }
+
+
+class CommandLineOptions
+{
+    [Option("path", Required = true, HelpText = "Write path to file")]
+    public string Path { get; set; }
+
+
+}
+
